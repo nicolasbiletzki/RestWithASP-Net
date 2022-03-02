22f91c6101b8777e2abe66425b7272780bc6f144
Nicolas Biletzki <nicolasbiletzki@gmail.com>
2022-03-02 12:09:15 -0300

    Implementing IPersonService Interface

---
+++ RestWithASP-NET/Services/Implementations/PersonServiceImplementation.cs
@@ -0,0 +1,71 @@
+﻿using RestWithASP_NET.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace RestWithASP_NET.Services.Implementations
+{
+    public class PersonServiceImplementation : IPersonService
+    {
+        private volatile int count;
+        public Person Create(Person person)
+        {
+            return person;
+        }
+
+        public void Delete(long id)
+        {
+
+        }
+
+        public List<Person> FindAll()
+        {
+            List<Person> persons = new List<Person>();
+            for(int i = 0; i < 8; i++)
+            {
+                Person person = MockPerson(i);
+                persons.Add(person);
+            }
+            return persons;
+        }
+
+        private Person MockPerson(int i)
+        {
+            return new Person
+            {
+                Id = IncrementAndGet(),
+                FirstName = "Person Name" + i,
+                LastName = "Person LastName" + i,
+                Address = "Some Address" + i,
+                Gender = "Male"
+
+            };
+        }
+
+        private long IncrementAndGet()
+        {
+            return Interlocked.Increment(ref count);
+        }
+
+        public Person FindByID(long id)
+        {
+            return new Person
+            {
+                Id = 1,
+                FirstName = "Nicolas",
+                LastName = "Biletzki",
+                Address = "Curitiba - Parana",
+                Gender = "Male"
+
+            };
+        }
+
+        public Person Update(Person person)
+        {
+
+            return person;
+        }
+    }
+}
